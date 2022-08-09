8a7721f27bb4c58278e31f6666dbe06ed2c6326a
Kevin <magavakevin10@gmail.com>
2022-08-08 20:15:28 -0400

    commit tree b239352f8cc2f9577dfddc6b67911588425471d4

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({onItemFormSubmit}) {
+  const [newCategory, setNewCategory] = useState("Produce")
+  const [newItemName, setNewItemName] = useState("")
+  const newItem = {
+    id: uuid(), // the `uuid` library can be used to generate a unique id
+    name: newItemName,
+    category: newCategory,
+  }
+
+  function handleNewCategory(e){
+    setNewCategory(e.target.value)
+  }
+
+  function handleNewItemName(e){
+    setNewItemName(e.target.value)
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    onItemFormSubmit(newItem)
+  }
+
   return (
-    <form className="NewItem">
+    //we need the handlesubmit on the form itself for the e.preventdefault().
+    //within the handleSubmit we need a function to call on ItemFormSubmit
+    //ItemFormSubmit comes from the App Parent component
+    //New item is passed to the parent
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" onChange={handleNewItemName}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" onChange={handleNewCategory}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
